52cddb4f7c9ab3b9681c76d4a5167fb87d2d35ff
arwatanveer7 <arwa.tanveer77@gmail.com>
2024-08-31 14:20:49 +0500

    Add files via upload

---
+++ Code for Client-Multi-Threaded server-DEP-Task 4.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <winsock2.h>
+#include <thread>
+
+#define PORT 8080
+
+void handleClient(SOCKET clientSocket) {
+    char buffer[1024] = {0};
+    int bytesReceived = recv(clientSocket, buffer, sizeof(buffer) - 1, 0);
+
+    if (bytesReceived > 0) {
+        buffer[bytesReceived] = '\0';
+        std::cout << "Received: " << buffer << std::endl;
+
+        const char* response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nMessage received!";
+        send(clientSocket, response, strlen(response), 0);
+    } else {
+        std::cerr << "Failed to receive data from client" << std::endl;
+    }
+
+    closesocket(clientSocket);
+}
+
+int main() {
+    WSADATA wsaData;
+    SOCKET serverSocket, clientSocket;
+    struct sockaddr_in serverAddr, clientAddr;
+    int clientAddrSize = sizeof(clientAddr);
+
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        std::cerr << "WSAStartup failed" << std::endl;
+        return 1;
+    }
+
+    serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (serverSocket == INVALID_SOCKET) {
+        std::cerr << "Socket creation failed" << std::endl;
+        WSACleanup();
+        return 1;
+    }
+
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+    serverAddr.sin_port = htons(PORT);
+
+    if (bind(serverSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
+        std::cerr << "Bind failed" << std::endl;
+        closesocket(serverSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    if (listen(serverSocket, SOMAXCONN) == SOCKET_ERROR) {
+        std::cerr << "Listen failed" << std::endl;
+        closesocket(serverSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    std::cout << "Server listening on port " << PORT << std::endl;
+
+    while (true) {
+        clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddr, &clientAddrSize);
+        if (clientSocket == INVALID_SOCKET) {
+            std::cerr << "Accept failed" << std::endl;
+            continue;
+        }
+
+        std::thread clientThread(handleClient, clientSocket);
+        clientThread.detach();
+    }
+
+    closesocket(serverSocket);
+    WSACleanup();
+
+    return 0;
+}
+
+
+
+
+
